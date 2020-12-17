ade01ff11e286610edb46dd5cc7c3da561772c18
rayush333 <72930101+rayush333@users.noreply.github.com>
2020-12-17 20:52:28 +0530

    Add files via upload

---
+++ raychat-frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import reducer, {initialState} from "./components/reducer";
+import {StateProvider} from "./components/StateProvider";
+ReactDOM.render(
+<StateProvider initialState={initialState} reducer={reducer}>
+<App />
+</StateProvider>,document.getElementById("root"));
